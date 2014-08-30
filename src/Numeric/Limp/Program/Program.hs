d82aba872788c380e3a723668a7734a2fb6cf631
Amos Robinson <amos.robinson@gmail.com>
2014-08-30 12:39:15 +0200

    helpers for constructing programs

--- src/Numeric/Limp/Program/Program.hs
+++ src/Numeric/Limp/Program/Program.hs
@@ -5,6 +5,7 @@ import Numeric.Limp.Program.Bounds
 import Numeric.Limp.Program.Constraint
 import Numeric.Limp.Program.Linear
 import Numeric.Limp.Program.ResultKind
+import Numeric.Limp.Rep
 
 -- | Direction to optimise program in: minimise or maximise.
 data Direction
@@ -19,14 +20,27 @@ data Direction
 --
 data Program z r c
  = Program {
-   -- | The objective function
-     _objective     :: Linear z r c KR
    -- | Optimisation direction
-   , _direction     :: Direction
+     _direction     :: Direction
+   -- | The objective function
+   , _objective     :: Linear z r c KR
    -- | All constraints bundled up with @:&&@.
    , _constraints   :: Constraint z r c
    -- | Upper and lower bounds of variables.
    -- Not all variables need to be mentioned, and if variables are mentioned multiple times, the intersection is used.
    , _bounds        :: [Bounds z r c]
    }
+
+program :: Rep c => Direction -> Linear z r c k -> Constraint z r c -> [Bounds z r c] -> Program z r c
+program dir obj constr bounds
+ = Program dir (toR obj) constr bounds
+
+minimise :: Rep c => Linear z r c k -> Constraint z r c -> [Bounds z r c] -> Program z r c
+minimise
+ = program Minimise
+ 
+
+maximise :: Rep c => Linear z r c k -> Constraint z r c -> [Bounds z r c] -> Program z r c
+maximise
+ = program Maximise
 
