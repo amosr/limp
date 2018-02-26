d01d5f7b87ba27d2cc59592e4f0d1966f42f0dc4
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-02-26 15:25:28 +0900

    Add missing Semigroup instances

--- src/Numeric/Limp/Program/Constraint.hs
+++ src/Numeric/Limp/Program/Constraint.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE CPP #-}
 module Numeric.Limp.Program.Constraint where
 import Numeric.Limp.Program.Linear
 import Numeric.Limp.Program.ResultKind
 import Numeric.Limp.Rep
 
+#if MIN_VERSION_base(4,9,0) && !MIN_VERSION_base(4,11,0)
+import Data.Semigroup
+#endif
 import Data.Monoid
 
 -- | Different kind of constraints.
@@ -22,7 +26,7 @@ import Data.Monoid
 --   [@:>@]     Strictly greater than: this is only allowed for purely integer functions
 --
 --   [@Between@] @Between a b c@ is equivalent to @a :<= b :&& b :<= c@
--- 
+--
 --   [@:&&@]    Conjunction of two constraints
 --
 --   [@:!@]     @"name" :! constr@ Annotate a constraint with a name, or other useless information
@@ -51,7 +55,12 @@ infix  5 :>
 infix  4 :!
 infixr 3 :&&
 
+#if MIN_VERSION_base(4,9,0)
+instance Semigroup (Constraint z r c) where
+ (<>) = (:&&)
+#endif
+
+
 instance Monoid (Constraint z r c) where
  mempty  = CTrue
  mappend = (:&&)
-
